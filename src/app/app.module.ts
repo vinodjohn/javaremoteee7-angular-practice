07d98154761d52c96f6287b63c21ef4884397a7e
Vinod John <vinodinfantdass.john-rozario@swedbank.ee>
2021-10-09 14:11:05 +0300

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import {FormsModule} from "@angular/forms";
 import {TeacherModule} from "./teacher/teacher.module";
 import {RouterModule, Routes} from "@angular/router";
 import {TeacherDashboardComponent} from "./teacher/teacher-dashboard/teacher-dashboard.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 const appRoutes: Routes =
     [
@@ -29,7 +35,13 @@ const appRoutes: Routes =
         BrowserModule,
         FormsModule,
         TeacherModule,
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(appRoutes),
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule
     ],
     providers: [],
     bootstrap: [AppComponent]
